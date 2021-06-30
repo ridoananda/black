93fcc8d38ca4f5d2c89b5d9e3704f582d478d3b4
unknown <ridoananda123@gmail.com>
2021-06-30 20:04:26 +0700

    memperbaiki style dan menambahkan animasi scroll to top

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', (e) => {
+  const btnScrollTop = document.querySelector('.scroll-top');
+  if (this.scrollY >= 200) {
+    btnScrollTop.classList.add('show')
+  } else {
+    btnScrollTop.classList.remove('show')
+  }
+})
